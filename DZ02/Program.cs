1c407f5fa177ae378480c5353eb6b4dd8adb0df8
Semyon <tonkih.sem@mail.ru>
2022-10-26 22:36:34 +0500

    added DZ02

---
+++ DZ02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите число M: ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int userNumberM, int userNumberN)
+{
+    if (userNumberM == userNumberN)
+    {
+        return userNumberN;
+    }
+    return userNumberN + GetSum(userNumberM, userNumberN - 1);
+}
+
+int result = GetSum(userNumberM,userNumberN);
+Console.WriteLine(result);
+
